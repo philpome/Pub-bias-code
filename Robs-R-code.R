5630cea2c790afcb21f2a356e39546ef9dbe907b
Robert Tunison <tunisorp@email.uc.edu>
2017-10-23 13:30:18 -0400

    Changes in R script to change pathway of document to the folder of the Rproject (working directory is current folder). Code from experimental-code3 was merged into the script to reflect changes in how the aggregated dataframe was structured.

--- Robs-R-code.R
+++ Robs-R-code.R
@@ -3,7 +3,7 @@ require(ggplot2)
 require(lme4)
 require(MASS)
 
-pbdata <- fromJSON('~/Documents/PubBiasJazz/Pub-bias-code/workingDataset.json')
+pbdata <- fromJSON('workingDataset.json')
 pbdata <- pbdata$data
 attach(pbdata)
 
@@ -14,56 +14,74 @@ pbdata$AI <- as.numeric(pbdata$AI)
 pbdata$PlantsTotal <- as.numeric(pbdata$PlantsTotal)
 pbdata$PlantsEndemic <- as.numeric(pbdata$PlantsEndemic)
 
+#Create empty vector for each of the columns in the aggregated dataframe.
+#These will be filled with the for loop based on how we want them aggregated (sum, mean, etc...).
+np.country <- vector()
+np.development <- vector()
+np.region <- vector()
+np.subregion <- vector()
+np.LandArea <- vector()
+np.gbifDiversity <- vector()
+np.DI <- vector()
+np.AI <- vector()
+np.PlantsTotal <- vector()
+np.PlantsEndemic <- vector()
+np.authorRepresentation <- vector()
+np.mismatch <- vector()
+np.allMismatch <- vector()
 
-numpubs <- aggregate(cbind(`Plant species (higher), threatened`,gbifDiversity,authorRepresentation) ~ country + development,pbdata,sum)
+#For loop that creates an index number for each country, 
+#then aggregates the temporal data to country summaries based on how we want that data aggregated (sum,mean,factor).
 
-numpubs$gbifDiversity <- numpubs$gbifDiversity/38
+for (i in seq(length(unique(pbdata$country)))) { 
+  
+  np.country[i] <- unique(pbdata$country)[i]
+  np.development[i] <- unique(pbdata$development[pbdata$country==unique(pbdata$country)[i]])
+  np.region[i] <- unique(pbdata$region[pbdata$country==unique(pbdata$country)[i]])
+  np.subregion[i] <- unique(pbdata$subregion[pbdata$country==unique(pbdata$country)[i]])
+  np.LandArea[i] <- mean(pbdata$`Land area (sq. km)`[pbdata$country==unique(pbdata$country)[i]],na.rm=TRUE)
+  np.gbifDiversity[i] <- mean(pbdata$gbifDiversity[pbdata$country==unique(pbdata$country)[i]],na.rm=TRUE)
+  np.DI[i] <- mean(pbdata$DI[pbdata$country==unique(pbdata$country)[i]],na.rm=TRUE)
+  np.AI[i] <- mean(pbdata$AI[pbdata$country==unique(pbdata$country)[i]],na.rm=TRUE)
+  np.PlantsTotal[i] <- mean(pbdata$PlantsTotal[pbdata$country==unique(pbdata$country)[i]],na.rm=TRUE)
+  np.PlantsEndemic[i] <- mean(pbdata$PlantsEndemic[pbdata$country==unique(pbdata$country)[i]],na.rm=TRUE)
+  np.authorRepresentation[i] <- sum(pbdata$authorRepresentation[pbdata$country==unique(pbdata$country)[i]],na.rm=TRUE)
+  np.mismatch[i] <- sum(pbdata$mismatch[pbdata$country==unique(pbdata$country)[i]],na.rm=TRUE)
+  np.allMismatch[i] <- sum(pbdata$allMismatch[pbdata$country==unique(pbdata$country)[i]],na.rm=TRUE)
+  
+}
 
-numpubs$tOd <- numpubs$`Plant species (higher), threatened`/numpubs$gbifDiversity
+#Data columns are then bound to a dataframe
+np <- data.frame(country = np.country,
+                 development = np.development,
+                 region = np.region,
+                 subregion = np.subregion,
+                 LandArea = np.LandArea,
+                 gbifDiversity = np.gbifDiversity,
+                 DI = np.DI,
+                 AI = np.AI,
+                 PlantsTotal = np.PlantsTotal,
+                 PlantsEndemic = np.PlantsEndemic,
+                 authorRepresentation = np.authorRepresentation,
+                 mismatch = np.mismatch,
+                 allMismatch = np.allMismatch)
 
-test<-ggplot(pbdata, aes(`Plant species (higher), threatened`,mismatch,color=factor(subregion)))+geom_jitter()
-test
+#create column that shows collaboration by subtracting allMismatches from mismatches
+np$collab <- np$mismatch - np$allMismatch
 
-#change NA's in the dataset to 0's (may need to do for authorRep too)
-pbdata$`Plant species (higher), threatened`[is.na(pbdata$`Plant species (higher), threatened`)]<-0
+#Linear models of richness as a function of area are used to find the equation of a best fit line.
+#We can use that best fit line to determine the expected richness based on the area of the country.
+#If the variation from the expected biodiversity shows a relative richness normalizing for land area.
 
-#sum all the publications by for each country by year
-nump<-aggregate(authorRepresentation~country, pbdata, sum)
+lm(log(np$gbifDiversity)~log(np$LandArea))
+lm(log(np$PlantsTotal)~log(np$LandArea))
 
-#sum all the threatened plants by year
-planty<-aggregate(`Plant species (higher), threatened`~country,pbdata,sum)
+np$GBIFexpectedDiv <- exp(4.9644 + (0.2816*log(np$LandArea))) ##based on linear model with gbif data
+np$GBIFdeviance <- log(np$gbifDiversity) - log(np$GBIFexpectedDiv)   ##gbif richness minus expected richness based on area
 
-#sum all the gbif data by year
-bify<-aggregate(gbifDiversity~country,pbdata,mean)
+np$UNexpectedDiv <- exp(4.4798 + (0.3074*log(np$LandArea))) ##based on linear model with UN Data
+np$UNdeviance <- log(np$PlantsTotal) - log(np$UNexpectedDiv)   ##Un plant species richness minus expected richness based on area
 
-#merge all the datasets together
-numplanty<-merge(nump,planty,by="country")
-pubsbydiv<-merge(numplanty,bify,by="country")
-
-#create the biodiversity variable
-
-pubsbydiv$aggDiversity<-pubsbydiv$`Plant species (higher), threatened`/pubsbydiv$gbifDiversity
-plot(pubsbydiv$authorRepresentation~pubsbydiv$aggDiversity)
-
-#test for correlation
-
-cor.test(pubsbydiv$authorRepresentation, pubsbydiv$aggDiversity, method="spearman")
-cor.test(pubsbydiv$authorRepresentation, pubsbydiv$aggDiversity, method="kendall")
-
-#that was dumb, try it with a linear model instead
-
-model1<-glm(authorRepresentation~aggDiversity, data=pubsbydiv, family="poisson")
-summary(model1)
-
-#that's overdispersed, try a quasi-poisson or negative binomial model
-
-model2<-glm(authorRepresentation~aggDiversity, data=pubsbydiv, family="quasipoisson")
-summary(model2)
-
-model3<-glm.nb(authorRepresentation~aggDiversity, data=pubsbydiv)
-summary(model3)
-
-#let's go with the nb model
 
 #Negative binomial model for author representation as a function of UN's diversity index
 model.nb.ar <- glm.nb(np$authorRepresentation ~ np$AI)
